d42bd07ba903a11b68092fe2e060c1f17c83fa21
net-runner <slazynski.j@gmail.com>
2022-01-28 17:30:11 +0100

    Repo restruct.

---
+++ RecipeLib.Tests/ControllerTests/ApiControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace RecipeLib.Tests.ControllerTests
+{
+    public class ApiControllerTests
+    {
+        [Fact]
+        public async Task ApiRoute_Should_Return_ResponseOk()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            var httpclient = factory.CreateClient();
+
+            var response = await httpclient.GetAsync("/api/");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
